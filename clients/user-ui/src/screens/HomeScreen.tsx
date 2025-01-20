1aedcad0f636b67e4357caeccfce9954816186ae
Baban <avhandy946@gmail.com>
2025-01-20 22:26:57 +0700

    frontend

---
+++ clients/user-ui/src/screens/HomeScreen.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Layout/Header";
+
+const HomeScreen = () => {
+  return (
+    <>
+      <div>
+        <Header />
+      </div>
+    </>
+  );
+};
+
+export default HomeScreen;
